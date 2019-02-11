6e2ff3cf0f1446d9391362628d5727bc27c9b497
nxie2 <47185714+nxie2@users.noreply.github.com>
2019-02-10 20:44:48 -0500

    Update nan.php

--- project2/nan.php
+++ project2/nan.php
@@ -1,1 +1,91 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "php/head.php";?>
+
+<body class="container">
+
+<header>
+
+  <a href="index.php">
+     <h1>Team NY</h1>
+     
+  </a>
+
+</header>
+
+<aside>
+<h2> Basic Information </h2>
+  <ul>
+     <li>Nan Xie</li>
+     <li>Lab03 Markdown assignment</li>
+     <li>Feb.6th 2019</li>
+     <li>Creatures at home</li>
+  </ul>
+
+</aside>
+
+<main>
+
+<h2>Animals that live at home</h2>
+<h3>Where in home do they live?</h3>
+
+<h4>Raccoons</h4>
+<p>Most commonly the attic, sometimes in the walls or under a house.</p>
+
+<h4>Squirrels</h4>
+<p>Most commonly the attic, but also very commonly in the eaves and soffits, and smaller areas like walls and small crawl spaces. Always high off ground level.</p>
+
+<h4>Rats and Mice</h4>
+<p>Anywhere and everywhere. Attic, walls, inside pipes, inside house, kitchen, etc.</p>
+
+<h2>Description of the animals</h2>
+
+<h3>Description of Raccoon</h3>
+
+<p>One of the most commonly dealt with nuisance animal species here in central Florida. They are very capable and adaptable creatures, and I consider them very intelligent and talented. They have very nimble hands, they are very strong, and they are excellent climbers. For these reasons, they commonly break into houses, where they like to live, raise young, and steal food. Many raccoons have no fear of people (or dogs), and thus commonly come into conflict with humans in urban settings.</p>
+
+<h3>Description of squirrels</h3>
+<p>Possibly the "classic" and most common nuisance species throughout North America, and Florida is no exception. The Eastern Gray Squirrel is incredibly agile and simply loves to live inside the attics of people's homes, and will chew its way inside. Squirrels have two litters of young per year, and prefer to raise them in a safe place, such as your attic. Once inside, these rodents, like all rodents, will often chew on wood and wires, not to mention the noise they make running around up there.</p>
+
+<h3>Description of Rats</h3>
+
+<p>Rats are probably the single most commonly dealt with nuisance animal here in Florida. The Roof Rat (rattus rattus), also known as the Citrus Rat here in FL, invades any home with any small opening leading inside. They usually enter merely for warmth and shelter, but also for food. Once inside, they make a lot of scurrying and scratching noises, but they also chew on wires, defecate and urinate everywhere, and often enter the home and raid the pantry. They are nocturnal, and amazingly fast and agile.</p>
+
+<h2>Common Characteristics</h2>
+
+ <ul>
+ 	<li>Each animals has a tail</li>
+ 	<li>Each animal has furry skin</li>
+ 	<li>Each Animal lives at home</li>
+  <li>Each animal has fur legs </li>
+ </ul>
+
+<h2>Common Question about these creatures</h2>
+<h3> How to keep rodents away from the attic?</h3>
+
+<p>As useful as the attic in our home is, it can also be a pest magnet. The darkness and protection against weather are just too tempting for rodents to pass by. Unfortunately, with rodents come other problems that can damage your property and even lower its value. If the material damage to your home is not enough of an incentive to try your best to keep these critters out of your house then you may also want to consider the damage to your health and that of your family and pets. Rodents can carry several diseases that can be even life threatening to you or to your furry best friend; so keeping mice, rats and other rodents out of your house should be a priority.</p>
+
+<h3>How Do You Keep Them Out?</h3>
+
+<p>The first thing to remember is that your home can be invaded at any time of the year. They can also fit through some of the smallest holes in your walls and the roof; this means looking for the tiny spaces where a rodent can make his way into your house or attic is the first step to keeping them out. Once you find a hole or crack you must seal it immediately. Also remember that rodent teeth are very sharp and that they can chew trough wood; so if there is already a hole on your wall or your roof then you may want to try to plug it up with metal or something that rodent’s teeth may have a hard time getting through.</p>
+
+<h3>How to make Your Attic “Not Rodent Friendly” ?</h3>
+<p>There are also some things you can do in your attic that will make it more unsuitable for rodents. Number one in that list is to keep it organized. Make sure that you organize it in a way that it is difficult for rodents to make a nest or hide. There is also natural rodent repellent, like for example mint plants. Mice are not fond of the smell of mint plants and will do anything to avoid it. Placing traps in different places is also a good idea but the only problem with that is that you’ll need to check them constantly to make sure you keep your attic clean of bodies and free of smells. You also have the choice to place poison but if you have household pets you have to make sure that they do not find it. The poison must be easy to find for the rodent but impossible to find for your dog or cat.</p>
+
+<h3>Why It’s Important To Keep Them Out In The First Place?</h3>
+<p>You do not want rodents getting into your house for numerous reasons. Following preventative measures can help. But what if rodents are in your house already? You can end up with an infestation in a brief period of time. Rodents reproduce easily and where there are two there will be more in no time. Because of the fast reproduction of rodents, the damage caused to your house can be a lot bigger than just a hole in the wall. Rodents can chew through wood or important wires. They can also leave a lot of droppings throughout your house and that can make your family or pets ill. Additionally, they die in your house and if you do not know where they are you will have a nasty smell throughout a big section of your house for a long time.</p>
+
+
+</main>
+
+
+
+<?php include "php/footer.php";?>
+
+
+
+
+</body>
+</html>
