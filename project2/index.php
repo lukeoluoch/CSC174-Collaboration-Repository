aa08e5f003ab4a400228305217c64984cfc2198f
nxie2 <47185714+nxie2@users.noreply.github.com>
2019-02-10 20:46:15 -0500

    Update index.php

--- project2/index.php
+++ project2/index.php
@@ -1,1 +1,45 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "php/head.php";?>
+
+<body class="container">
+
+<header>
+
+  <a href="index.php">
+     <h1>Team NY</h1>
+  </a>
+
+</header>
+
+<aside>
+
+  <ul>
+     <li>Ming Yuan markdown website</li>
+     <li>Nan Xie markdown website</li>
+     <li>Shawn Chen markdown website</li>
+  </ul>
+
+</aside>
+
+
+<main>
+
+<h2>This is Assignment 2</h2>
+
+<h2>City</h2>
+
+<p>New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.</p>
+
+
+</main>
+
+
+
+<?php include "php/footer.php";?>
+
+
+</body>
+</html>
