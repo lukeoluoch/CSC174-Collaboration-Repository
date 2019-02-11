c30d1d28cf3e977a431b2be33b5f0c4f247180a6
Mingyuan Shan <mingyuan.shan@rochester.edu>
2019-02-10 22:49:32 -0500

    index description

--- project2/index.php
+++ project2/index.php
@@ -12,31 +12,34 @@
 
 <header>
 
-  <a href="index.php">
-     <h1>Team NY</h1>
-  </a>
+    <a href="index.php">
+        <h1>Team NY</h1>
+    </a>
 
 </header>
 
 <aside>
 
-  <ul>
-     <li>Ming Yuan markdown website</li>
-     <li>Nan Xie markdown website</li>
-     <li>Shawn Chen markdown website</li>
-  </ul>
+    <ul>
+        <li>Ming Yuan markdown website</li>
+        <li>Nan Xie markdown website</li>
+        <li>Shawn Chen markdown website</li>
+    </ul>
 
 </aside>
 
 
 <main>
 
-<h2>This is Assignment 2</h2>
+    <h2>This is Assignment 2</h2>
 
-<h2>City</h2>
+    <h2>City</h2>
 
-<p>New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.</p>
+    <p>New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.</p>
 
+    <h2>Contents</h2>
+
+    <p>This is a website of animals, musicians and video games.</p>
 
 </main>
 
