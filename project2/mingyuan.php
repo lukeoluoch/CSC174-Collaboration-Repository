3494b75db3d2472417e434857554ae851801cce8
nxie2 <47185714+nxie2@users.noreply.github.com>
2019-02-10 20:45:44 -0500

    Update mingyuan.php

--- project2/mingyuan.php
+++ project2/mingyuan.php
@@ -1,1 +1,74 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "php/head.php";?>
+
+<body class="container">
+
+<header>
+
+  <a href="index.php">
+     <h1>Team NY</h1>
+     
+  </a>
+
+</header>
+
+<aside>
+<h1> Iconic Video Game </h1>
+  <ul>
+     <li>Mingyuan Shan</li>
+     <li>Assignment 3</li>
+     <li>Feb.6th 2019</li>
+  </ul>
+
+</aside>
+
+<main>
+
+<h2>What this page is about?</h2>
+<p>The three items in this website are all famous video games in their respective genres. They are similar obviously as they are all video games. They are all resaonably new members in the video game world. Dragon age: Origins came out in 2009. BioShock: Infinite came out in 2013. Divinity: Original Sin 2 came out in 2018. As video games, they all have a developer and a publisher. Developer is a team(s) that made the game from scratch. Publisher works to publish the game to different regions of the world and makes sure that they meet all regulations demanded in different institutions. </p>
+
+
+<h2>Dragon Age: Origins:</h2>
+<ul>
+  <li>Publisher: Electronic Arts</li>
+  <li>Developer: BioWare</li>
+</ul>
+
+<p>Summary: ***Dragon Age: Origins*** is a [role-playing game](https://en.wikipedia.org/wiki/Role-playing_game) developed by [BioWare](https://en.wikipedia.org/wiki/BioWare) and published by [Electronic Arts](https://en.wikipedia.org/wiki/Electronic_Arts). It is the first game in the *Dragon Age* franchise, and was released for [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows), [PlayStation 3](https://en.wikipedia.org/wiki/PlayStation_3), and [Xbox 360](https://en.wikipedia.org/wiki/Xbox_360) in November 2009, and for [Mac OS X](https://en.wikipedia.org/wiki/Mac_OS_X) in December 2009. Set in the fictional kingdom of Ferelden during a period of civil strife, the game puts the player in the role of a [warrior](https://en.wikipedia.org/wiki/Warrior_(character_class)), [mage](https://en.wikipedia.org/wiki/Wizard_(character_class)), or [rogue](https://en.wikipedia.org/wiki/Thief_(character_class)) coming from an [elven](https://en.wikipedia.org/wiki/Elves_in_fantasy_fiction_and_games#Elves_in_games), [human](https://en.wikipedia.org/wiki/Human), or [dwarven](https://en.wikipedia.org/wiki/Dwarf_(Germanic_mythology)) background. The player character is recruited into the Grey Wardens, an ancient order that stands against demonic forces known as "Darkspawn", and is tasked with defeating the Archdemon that commands them and ending their invasion. The game is played from a [third-person perspective](https://en.wikipedia.org/wiki/Third-person_perspective) that can be shifted to a [top-down perspective](https://en.wikipedia.org/wiki/Top-down_perspective). Throughout the game, players encounter various companions, who play major roles in the game's plot and gameplay.</p>
+
+
+
+<h2>Bioshock: Infinite</h2>
+ <ul>
+   <li>Publisher: 2k games</li>
+   <li>Developer: Irrational Games </li>
+ </ul>
+
+ <p>Summary: ***BioShock Infinite*** is a [first-person shooter](https://en.wikipedia.org/wiki/First-person_shooter) [video game](https://en.wikipedia.org/wiki/Video_game) developed by [Irrational Games](https://en.wikipedia.org/wiki/Irrational_Games) and published by [2K Games](https://en.wikipedia.org/wiki/2K_Games). It was released worldwide for the [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows), [PlayStation 3](https://en.wikipedia.org/wiki/PlayStation_3), [Xbox 360](https://en.wikipedia.org/wiki/Xbox_360), and [OS X](https://en.wikipedia.org/wiki/OS_X) platforms in 2013, and a [Linux](https://en.wikipedia.org/wiki/Linux) port was released in 2015. *Infinite* is the third installment in the [*BioShock* series](https://en.wikipedia.org/wiki/BioShock_(series)), and though it is not immediately part of the storyline of previous *BioShock* games, it features similar gameplay concepts and themes. Irrational Games and creative director [Ken Levine](https://en.wikipedia.org/wiki/Ken_Levine_(game_developer))based the game's setting on historical events at the turn of the 20th century, such as the 1893 [World's Columbian Exposition](https://en.wikipedia.org/wiki/World%27s_Columbian_Exposition), and based the story on the concept of [American exceptionalism](https://en.wikipedia.org/wiki/American_exceptionalism), while also incorporating influences from more recent events at the time such as the 2011 [Occupy movement](https://en.wikipedia.org/wiki/Occupy_movement).</p>
+
+
+
+<h2>Divinity: Original Sin 2</h2>
+
+ <ul>
+ 	<li>Publisher: Bandai Namco Entertainment</li>
+   <li>Developer: Larian Studios</li>
+ 	
+ </ul>
+
+<p>Summary: ***Divinity: Original Sin II*** is a [role-playing video game](https://en.wikipedia.org/wiki/Role-playing_video_game) developed and published by [Larian Studios](https://en.wikipedia.org/wiki/Larian_Studios). The sequel to 2014's *Divinity: Original Sin*, it was released for [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows) in September 2017, for [PlayStation 4](https://en.wikipedia.org/wiki/PlayStation_4) and [Xbox One](https://en.wikipedia.org/wiki/Xbox_One), alongside an enhanced edition for Windows, in August 2018. In January 2019 it was released for [macOS](https://en.wikipedia.org/wiki/MacOS). The game received universal acclaim, with many critics praising its complexity and interactivity, considering it to be one of the best role-playing games of all time. It was also a commercial success, selling over a million copies in two months.</p>
+
+</main>
+
+
+
+<?php include "php/footer.php";?>
+
+
+
+
+</body>
+</html>
