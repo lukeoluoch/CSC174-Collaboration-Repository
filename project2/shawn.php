938892aea69bfe498900f87c53c943aa0bf95e80
nxie2 <47185714+nxie2@users.noreply.github.com>
2019-02-10 20:45:19 -0500

    Update shawn.php

--- project2/shawn.php
+++ project2/shawn.php
@@ -1,1 +1,97 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "php/head.php";?>
+
+<body class="container">
+
+<header>
+
+  <a href="index.php">
+     <h1>Team NY</h1>
+     
+  </a>
+
+</header>
+
+
+
+<aside>
+<h1>British Musician/Band</h1>
+  <ul>
+     <li>Shawn Chen</li>
+     <li>CSC 174 in-class Lab 3</li>
+     <li>Feb.6th 2019</li>
+  </ul>
+
+</aside>
+
+
+
+<main>
+
+<h2>Things in common</h2>
+
+<p>Britain has a rich and varied musical history ranging from Church music to folk music to instrumentation.
+
+The nation is also known for its legacy in folk music which was revived in the mid-20th century, keeping folk music an important sub-culture within society.
+
+In the contemporary era, many forms of popular music, including folk music, jazz, rapping/hip hop, pop and rock music, have flourished in Britain. In fact the nation produced the band, The Beatles, with members John Lennon, Paul McCartney, George Harrison and Ringo Starr who took the musical world by storm and went on to become the foremost and most influential act of the rock era. Adele, Ed Sheeran, Sam Smith, and George Ezra are popular in the contemporary era. Below are examples of some famous British artists.</p>
+
+<p>New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.</p>
+
+
+<h2>Coldplay</h2>
+
+<p>Coldplay are a British rock band formed in London in 1996. The four members, lead singer and pianist Chris Martin, lead guitarist Jonny Buckland, bassist Guy Berryman, and drummer Will Champion were at University College London, and came together from 1996 to 1998, during which time the band changed names from Pectoralz, to Starfish, then Coldplay. Creative director and former manager Phil Harvey is often referred to as the fifth member by the band.
+
+Coldplay have won numerous awards throughout their career, including nine Brit Awards, six MTV Video Music Awards, seven MTV Europe Music Awards and seven Grammy Awards from 29 nominations. They have sold more than 100 million records worldwide, making them one of the world's best-selling music artists. Three of their albums: Parachutes, A Rush of Blood to the Head, and X&Y, are among the best-selling albums in UK chart history.</p>
+
+
+<h3>Band Members</h3>
+<ul>
+	<li>Chris Martin</li>
+	<li>Jonny Buckland</li>
+	<li>Guy Berryman</li>
+	<li>Will Champion</li>
+</ul>
+
+<h2>Tom Odell</h2>
+
+<p>Thomas Peter Odell (born 24 November 1990) is an English singer-songwriter. He released his debut extended play, Songs from Another Love, in 2012 and won the BRITs Critics' Choice Award in early 2013. His debut studio album, Long Way Down, was issued on 24 June 2013. His second studio album, Wrong Crowd, was released on 10 June 2016. In October 2018, his third studio album, Jubilee Road was released.
+
+Odell grew up listening to Elton John. One of the first albums he listened to was Goodbye Yellow Brick Road (1973). He also cites David Bowie, Jeff Buckley, Bob Dylan, Arthur Russell, Leonard Cohen, Leon Russell, Billy Joel, Randy Newman, Tom Waits, Rodríguez and Bruce Springsteen as influences.</p>
+
+<h3>Tours and Concerts</h3>
+ <ul>
+ 	<li>Long Way Down Tour (2013)</li>
+ 	<li>No Bad Days Tour (2016-2017)</li>
+ 	<li>Jubilee Road Tour (2018-2019)</li>
+ </ul>
+
+<h2>James Arthur</h2>
+<p>James Andrew Arthur (born 2 March 1988)is an English singer and songwriter.
+
+Arthur won the ninth series of The X Factor in 2012. His debut single, "Impossible", was released by Syco Music after the final and debuted at number one on the UK Singles Chart in its first week of release. Since then, it has gone on to sell over 1.4 million copies in the UK, making it the most successful winner's single in the show's history and has sold over 2.5 million copies worldwide. The follow-up single "You're Nobody 'til Somebody Loves You" reached number 2 on the UK Singles Chart. He released his self-titled debut studio album in November 2013. The album debuted and peaked at number two on the UK Albums Chart. After the release of his debut studio album James Arthur, he was involved in a series of controversies which led iTunes to offer refunds for the album.
+
+He has cited Nirvana lead singer and songwriter Kurt Cobain as the reason he became interested in music and also draws from many different genres from Hip Hop, to Soul, to R&B, to Rock, and artists like Prince, Miguel and Michael Jackson.
+
+Further influences include Marvin Gaye, Stevie Wonder, Boyz II Men, Black Sabbath, AC/DC and Bryan Adams.[97] He has also stated that he would love to collaborate with American rapper Eminem.</p>
+</main>
+
+<h3>Discography</h3>
+
+<ul>
+	<li>Sins by the sea (2011)</li>
+	<li>James Arthur (2013)</li>
+	<li>Back from the Edge (2016)</li>
+</ul>
+
+<?php include "php/footer.php";?>
+
+
+
+
+</body>
+</html>
