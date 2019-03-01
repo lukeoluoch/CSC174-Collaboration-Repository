4211b9102a9a223e4ab721ae9afcd48e5f6d1518
lukeoluoch <47185625+lukeoluoch@users.noreply.github.com>
2019-03-01 15:46:56 -0500

    fixes

--- project2/index.php
+++ project2/index.php
@@ -4,57 +4,81 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Home | New York | Assignment 03</title>
+    <title>Mingyuan | New York | Assignment 03</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body class="container">
 
 <header>
-
-
-  <a href="index.php">
-     <h1>Team NY</h1>
-     <img src="images/ny.png" alt="New York">
-  </a>
-
-
-
+<h1 class="pagetitle">Iconic Video Games</h1>
+<img src="images/dv2.png" alt="Divinity 2">  
 </header>
 
 <aside>
-
+  <div id="top-left">
+  <a href="index.php">
+     <h1>Team NY</h1>   
+  </a>
+  <ul>
+     <li>Mingyuan Shan</li>
+     <li>Assignment 3</li>
+     <li>Feb.6th 2019</li>
+  </ul>
+</div>
+<div id="top-right">
+  <nav>
     <ul>
-        <li>Ming Yuan markdown website</li>
-        <li>Nan Xie markdown website</li>
-        <li>Shawn Chen markdown website</li>
+        <li><a href="http://csc174.org/assignment03/newyork/mingyuan.php">Original Website</a></li>
     </ul>
-
+</nav>
+</div>
 </aside>
-
 
 <main>
 
+<h2>What this page is about?</h2>
+<p>The three items in this website are all famous video games in their respective genres. They are similar obviously as they are all video games. They are all resaonably new members in the video game world. Dragon Age: Origins came out in 2009. BioShock: Infinite came out in 2013. Divinity: Original Sin 2 came out in 2018. As video games, they all have a developer and a publisher. Developer is a team(s) that made the game from scratch. Publisher works to publish the game to different regions of the world and makes sure that they meet all regulations demanded in different institutions. </p>
 
-<h2>This is Assignment 3</h2>
+<div class ="content">
+<div id="dragon" class="games">
+<h2>Dragon Age: Origins</h2>
+<ul>
+  <li>Publisher: Electronic Arts</li>
+  <li>Developer: BioWare</li>
+</ul>
 
+<p>Summary: Dragon Age: Origins is a role-playing game developed by BioWare and published by Electronic Arts. It is the first game in the Dragon Age franchise, and was released for Microsoft Windows, PlayStation 3, and Xbox 360 in November 2009, and for Mac OS X in December 2009. Set in the fictional kingdom of Ferelden during a period of civil strife, the game puts the player in the role of a warrior, mage, or rogue coming from an elven, human, or dwarven background. The player character is recruited into the Grey Wardens, an ancient order that stands against demonic forces known as "Darkspawn", and is tasked with defeating the Archdemon that commands them and ending their invasion. The game is played from a third-person perspective that can be shifted to a top-down perspective. Throughout the game, players encounter various companions, who play major roles in the game's plot and gameplay.</p>
+</div>
 
-    <h2>About the City</h2>
+<div id="bioshock" class="games">
+<h2>Bioshock: Infinite</h2>
+ <ul>
+   <li>Publisher: 2k games</li>
+   <li>Developer: Irrational Games </li>
+ </ul>
 
+<p>Summary: BioShock Infinite is a first-person shooter video game developed by Irrational Games and published by 2K Games. It was released worldwide for the Microsoft Windows, PlayStation 3, Xbox 360, and OS X platforms in 2013, and a Linux port was released in 2015. Infinite is the third installment in the BioShock series, and though it is not immediately part of the storyline of previous BioShock games, it features similar gameplay concepts and themes. Irrational Games and creative director Ken Levine based the game's setting on historical events at the turn of the 20th century, such as the 1893 World's Columbian Exposition, and based the story on the concept of American exceptionalism, while also incorporating influences from more recent events at the time such as the 2011 Occupy movement.</p>
+</div>
 
-    <p>New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.</p>
+<div id="divinity" class="games">
+<h2>Divinity: Original Sin 2</h2>
 
+ <ul>
+ 	<li>Publisher: Bandai Namco Entertainment</li>
+   <li>Developer: Larian Studios</li>
+ 	
+ </ul>
 
-
-    <h2>Our Contents</h2>
-
-    <p>This is a website of animals, musicians and video games.</p>
-
+<p>Summary: Divinity: Original Sin II is a role-playing video game developed and published by Larian Studios. The sequel to 2014's Divinity: Original Sin, it was released for Microsoft Windows in September 2017, for PlayStation 4 and Xbox One, alongside an enhanced edition for Windows, in August 2018, and for macOS in January 2019. The game received universal acclaim, with many critics praising its complexity and interactivity, considering it to be one of the best role-playing games of all time. It was also a commercial success, selling over a million copies in two months.</p>
+</div>
+</div>
 </main>
 
 
 
 <?php include "inc/foot.inc";?>
+
 
 
 </body>
